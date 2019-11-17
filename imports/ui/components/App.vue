38424f9716259b02e3ebec2c1b08ea5b72b3662f
Brian Mulhall <brian.e.mulhall@gmail.com>
2019-11-17 12:41:05 -0600

    updating the formatting of the app.vue file

--- imports/ui/components/App.vue
+++ imports/ui/components/App.vue
@@ -1,32 +1,40 @@
 <template>
   <div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <router-link class="nav-link" :to="{ name: 'home' }" exact>Home</router-link>
-      </li>
-      <li class="nav-item">
-        <router-link class="nav-link" :to="{ name: 'books' }">Books</router-link>
-      </li>
-      <li class="nav-item">
-         <router-link class="nav-link" :to="{ name: 'address' }">Address</router-link>
-      </li>
-      <li class="nav-item">
-        <router-link class="nav-link" :to="{ name: 'live-odds' }">Bettings Odds</router-link>
-      </li>
-      <li class="nav-item">
-         <router-link class="nav-link" :to="{ name: 'person' }">Person</router-link>
-      </li>
-    </ul>
-  </div>
-</nav>
-<div class="container">
-    <router-view />
+      <a class="navbar-brand" href="/">Susco</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{ name: 'home' }" exact>Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{ name: 'books' }">Books</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{ name: 'address' }">Address</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{ name: 'live-odds' }">Bettings Odds</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{ name: 'person' }">Person</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container">
+      <router-view />
     </div>
   </div>
 </template>
@@ -35,18 +43,9 @@
 export default {
   data() {
     return {
-      date: new Date()
     };
   }
 };
 </script>
 
-<style>
-@import "{}/node_modules/vue-googlemaps/dist/vue-googlemaps.css";
-
-p {
-  font-size: 2em;
-  text-align: center;
-}
-
-</style>
+<style></style>
